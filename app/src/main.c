1d78bac77577a0251932b7c02ea6a0a5fd2f970e
Natalia Pluta <80521103+nmpluta@users.noreply.github.com>
2022-12-07 13:24:56 +0100

    3: Add basic can bus handling

--- app/src/main.c
+++ app/src/main.c
@@ -5,15 +5,27 @@
  */
 
 /**
- * @file 
+ * @file main.c
  * @brief Main function
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include "can_threads.h"
+#include <zephyr/drivers/can.h>
+
+#define SLEEP_TIME K_MSEC(250)
+
+extern const struct device * const can_dev;
 
 void main(void)
 {
+    can_init(can_dev);
+    can_threads_init();
+
     while (1)
     {
+        k_sleep(SLEEP_TIME);
     }
 }
